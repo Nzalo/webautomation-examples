3a6602d86441490892a618bb26def9c7203c59a4
Gonzalo <gonzalo.pa@protonmail.com>
2022-07-04 13:18:06 +0200

    first commit of working tool

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # webautomation-examples
 Web automation examples using Selenium and Helium
+
+## Installation
+* Clone this repo
+* Install `helium` using virtual environments as mentioned [here](https://github.com/mherrmann/selenium-python-helium) 
+* Install `python_dotenv` 
+* Install the browser and the [webdriver](https://www.selenium.dev/documentation/webdriver/) that you want to use (Chrome in thi case). For other browsers, modify the script.
+* Set up your own environment variables (see `.env.example`)
+* Run the script
+
+## Resources
+* [Working with environment variables in python](https://www.twilio.com/blog/environment-variables-python)
+* [Helium for selenium](https://github.com/mherrmann/selenium-python-helium)
---
+++ web-automation.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from helium import *
+import time
+import os
+
+def automation_ex1():
+    load_dotenv('.env')
+
+    start_chrome(os.environ['WEBADDRESS'])
+    click(os.environ['ABTEILUNG'])
+    time.sleep(1)
+
+    click('Ich habe den Hinweis gelesen')
+    click('Ich bin mit der Speicherung und Verarbeitung meiner Daten einverstanden.')
+    click('Weiter')
+    time.sleep(1)
+
+    write(os.environ['VORNAME'], into='Vorname')
+    write(os.environ['NACHNAME'], into='Nachname')
+    write(os.environ['EMAIL'], into='E-Mail')
+    click('Weiter')
+    time.sleep(1)
+
+    select(ComboBox("0"), "1")
+    click('Weiter')
+    time.sleep(1)
+
+    click('Termin suchen')
+    time.sleep(1)
+
+    click(os.environ['ABTEILUNG'])
+    click('Weiter')
+    time.sleep(1)
+
+    # if date is earlier than 01.AUGUST then book
+    click('Weiter')
+    time.sleep(1)
+
+    click('Termin verbindlich vereinbaren')
+    time.sleep(1)
+
+    time.sleep(5)
+    kill_browser()
+
+if __name__ == "__main__":
+    automation_ex1()
